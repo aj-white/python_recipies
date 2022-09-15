44957d5c04ce722b0accba22b4fef347963cb355
aj-white <72359843+aj-white@users.noreply.github.com>
2022-09-15 10:10:47 +0100

    add time_logger

--- profiling/time_and_profile.py
+++ profiling/time_and_profile.py
@@ -7,6 +7,34 @@ from functools import wraps
 from pstats import Stats
 from pstats import SortKey
 
+import logging
+
+logging.basicConfig(
+    filename="profile.log",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+)
+
+def time_logger(file: str):
+  """Logs profile times, enables adding labels for clearer logs when profiling methods"""
+    def timing(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            result = func(*args, **kwargs)
+            end = time.perf_counter()
+            print(f"Elapsed time {func.__name__}: {end - start} seconds")
+            elapsed = end - start
+            logging.info("%s - %s took %s seconds", file, func.__name__, elapsed)
+            return result
+
+        return wrapper
+
+    return timing
+
+
+
 
 def timing(func):
   """A Simple timing decorator"""
